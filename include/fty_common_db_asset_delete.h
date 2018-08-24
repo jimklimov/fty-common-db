bc0052b5f3297843385a9e46df03828d9987001f
Jana Rapava <janarapava@eaton.com>
2018-08-24 12:12:40 +0200

    Problem: We need to move DB delete functions here.
    Solution: Do it.
    
    Signed-off-by: Jana Rapava <janarapava@eaton.com>

--- include/fty_common_db_asset_delete.h
+++ include/fty_common_db_asset_delete.h
@@ -22,16 +22,71 @@
 #ifndef FTY_COMMON_DB_ASSET_DELETE_H_INCLUDED
 #define FTY_COMMON_DB_ASSET_DELETE_H_INCLUDED
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <tntdb/connect.h>
 
+namespace DBAssetsDelete {
 
+// delete_asset_link: delete src<->dst link
+// returns error if input params are unacceptable or error happened during delete
+    db_reply_t
+    delete_asset_link (tntdb::Connection &conn,
+                       uint32_t asset_element_id_src,
+                       uint32_t asset_element_id_dest);
 
-//  @end
+// delete_asset_links_to: delete all links which have given asset as 'dest'
+// returns error if input params are unacceptable or error happened during delete
+    db_reply_t
+    delete_asset_links_to (tntdb::Connection &conn,
+                           uint32_t asset_device_id);
 
-#ifdef __cplusplus
-}
-#endif
+///////////////////////////////////////////////////////////////////////////
+// delete_asset_group_links: delete all data about a group
+// returns error if input params are unacceptable or error happened during delete
+    db_reply_t
+    delete_asset_group_links (tntdb::Connection &conn,
+                              uint32_t asset_group_id);
 
+////////////////////////////////////////////////////////////////////////////
+// delete_asset_ext_attribute: delete given ext attribute of a given asset
+// returns error if input params are unacceptable or error happened during delete
+    db_reply_t
+    delete_asset_ext_attribute (tntdb::Connection &conn,
+                                const char *keytag,
+                                uint32_t asset_element_id);
+
+// delete_asset_ext_attributes_with_ro: delete all ext attributes of given asset with given 'read_only' status
+// returns error if input params are unacceptable or error happened during delete
+    db_reply_t
+    delete_asset_ext_attributes_with_ro (tntdb::Connection &conn,
+                                         uint32_t asset_element_id,
+                                         bool read_only);
+
+////////////////////////////////////////////////////////////////////////////
+// delete_asset_element: delete asset from t_bios_asset_element
+// returns error if input params are unacceptable or error happened during delete
+    db_reply_t
+    delete_asset_element (tntdb::Connection &conn,
+                          uint32_t asset_element_id);
+
+/////////////////////////////////////////////////////////////////////////////
+// delete_asset_element_from_asset_groups: delete asset from all group
+// returns error if input params are unacceptable or error happened during delete
+    db_reply_t
+    delete_asset_element_from_asset_groups (tntdb::Connection &conn,
+                                            uint32_t asset_element_id);
+
+// delete_asset_element_from_asset_groups: delete asset from specified group
+// returns error if input params are unacceptable or error happened during delete
+    db_reply_t
+    delete_asset_element_from_asset_group (tntdb::Connection &conn,
+                                           uint32_t asset_group_id,
+                                           uint32_t asset_element_id);
+
+///////////////////////////////////////////////////////////////////////////
+// delete_monitor_asset_relation_by_a: delete given asset from t_bios_monitor_asset_relation
+// returns error if input params are unacceptable or error happened during delete
+    db_reply_t
+    delete_monitor_asset_relation_by_a (tntdb::Connection &conn,
+                                        uint32_t id);
+} // end namespace
 #endif
--- src/fty_common_db_asset_delete.cc
+++ src/fty_common_db_asset_delete.cc
@@ -26,4 +26,515 @@
 @end
 */
 
+#include <fty_common_asset_types.h>
 #include "fty_common_db_classes.h"
+
+namespace DBAssetsDelete {
+
+// ATTENTION: in theory there could exist more than one link
+// between two devices
+db_reply_t
+delete_asset_link (tntdb::Connection &conn,
+                   uint32_t asset_element_id_src,
+                   uint32_t asset_element_id_dest)
+{
+    LOG_START;
+    log_debug ("  asset_element_id_src = %" PRIu32, asset_element_id_src);
+    log_debug ("  asset_element_id_dest = %" PRIu32, asset_element_id_dest);
+
+    db_reply_t ret = db_reply_new();
+
+    // input parameters control
+    if ( asset_element_id_src <= 0 )
+    {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_BADINPUT;
+        ret.msg        = "value <= 0 of asset_element_id_src is not allowed";
+        log_error ("end: %s, %s", "ignore delete", ret.msg.c_str());
+        return ret;
+    }
+    if ( asset_element_id_src <= 0 )
+    {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_BADINPUT;
+        ret.msg        = "value <= 0 of asset_element_id_src is not allowed";
+        log_error ("end: %s, %s", "ignore delete", ret.msg.c_str());
+        return ret;
+    }
+    log_debug ("input parameters are correct");
+
+    try{
+        tntdb::Statement st = conn.prepareCached(
+            " DELETE"
+            " FROM"
+            "   t_bios_asset_link"
+            " WHERE"
+            "   id_asset_device_src = :src AND"
+            "   id_asset_device_dest = :dest"
+        );
+
+        ret.affected_rows = st.set("src", asset_element_id_src).
+                               set("dest", asset_element_id_dest).
+                               execute();
+        log_debug ("[t_bios_asset_link]: was deleted %"
+                                    PRIu64 " rows", ret.affected_rows);
+        ret.status = 1;
+        LOG_END;
+        return ret;
+    }
+    catch (const std::exception &e) {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_INTERNAL;
+        ret.msg        = e.what();
+        LOG_END_ABNORMAL(e);
+        return ret;
+    }
+}
+
+db_reply_t
+delete_asset_links_to (tntdb::Connection &conn,
+                       uint32_t asset_device_id)
+{
+    LOG_START;
+    log_debug ("  asset_device_id = %" PRIu32, asset_device_id);
+
+    db_reply_t ret = db_reply_new();
+
+    // input parameters control
+    if ( asset_device_id <= 0 )
+    {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_BADINPUT;
+        ret.msg        = "value <= 0 of asset_device_id is not allowed";
+        log_error ("end: %s, %s", "ignore delete", ret.msg.c_str());
+        return ret;
+    }
+    log_debug ("input parameters are correct");
+
+    try{
+        tntdb::Statement st = conn.prepareCached(
+            " DELETE FROM"
+            "   t_bios_asset_link"
+            " WHERE"
+            "   id_asset_device_dest = :dest"
+        );
+
+        ret.affected_rows = st.set("dest", asset_device_id).
+                               execute();
+        log_debug ("[t_bios_asset_link]: was deleted %"
+                                PRIu64 " rows", ret.affected_rows);
+        ret.status = 1;
+        LOG_END;
+        return ret;
+    }
+    catch (const std::exception &e) {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_INTERNAL;
+        ret.msg        = e.what();
+        LOG_END_ABNORMAL(e);
+        return ret;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+db_reply_t
+delete_asset_group_links (tntdb::Connection &conn,
+                          uint32_t asset_group_id)
+{
+    LOG_START;
+    log_debug ("  asset_group_id = %" PRIu32, asset_group_id);
+
+    db_reply_t ret = db_reply_new();
+
+    // input parameters control
+    if ( asset_group_id <= 0 )
+    {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_BADINPUT;
+        ret.msg        = "0 value of asset_group_id is not allowed";
+        log_error ("end: %s, %s", "ignore delete", ret.msg.c_str());
+        return ret;
+    }
+    log_debug ("input parameters are correct");
+
+    try{
+        tntdb::Statement st = conn.prepareCached(
+            " DELETE FROM"
+            "   t_bios_asset_group_relation"
+            " WHERE"
+            "   id_asset_group = :grp"
+        );
+
+        ret.affected_rows = st.set("grp", asset_group_id).
+                               execute();
+        log_debug ("[t_bios_asset_group_relation]: was deleted %"
+                                PRIu64 " rows", ret.affected_rows);
+        ret.status = 1;
+        LOG_END;
+        return ret;
+    }
+    catch (const std::exception &e) {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_INTERNAL;
+        ret.msg        = e.what();
+        LOG_END_ABNORMAL(e);
+        return ret;
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+db_reply_t
+delete_asset_ext_attribute (tntdb::Connection &conn,
+                            const char *keytag,
+                            uint32_t asset_element_id)
+{
+    LOG_START;
+
+    db_reply_t ret = db_reply_new();
+
+    // input parameters control
+    if ( !persist::is_ok_keytag (keytag) )
+    {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_BADINPUT;
+        ret.msg        = "unexpected value of keytag";
+        log_error ("end: %s, %s", "ignore delete", ret.msg.c_str());
+        return ret;
+    }
+    if ( asset_element_id <= 0 )
+    {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_BADINPUT;
+        ret.msg        = "value <= 0 of asset_element_id is not allowed";
+        log_error ("end: %s, %s", "ignore delete", ret.msg.c_str());
+        return ret;
+    }
+    log_debug ("input parameters are correct");
+
+    try{
+        tntdb::Statement st = conn.prepareCached(
+            " DELETE FROM"
+            "   t_bios_asset_ext_attributes"
+            " WHERE"
+            "   keytag = :keytag AND"
+            "   id_asset_element = :element"
+        );
+
+        ret.affected_rows = st.set("keytag", keytag).
+                               set("element", asset_element_id).
+                               execute();
+        log_debug("[t_bios_asset_ext_attributes]: was deleted %"
+                                PRIu64 " rows", ret.affected_rows);
+        if ( ( ret.affected_rows == 1 ) || ( ret.affected_rows == 0 ) )
+        {
+            ret.status = 1;
+            LOG_END;
+            return ret;
+        }
+        else
+        {
+            ret.status     = 0;
+            ret.errtype    = DB_ERR;
+            ret.errsubtype = DB_ERROR_DELETEFAIL;
+            ret.msg        = "unexpected number of rows was deleted";
+            log_error ("end: %s", ret.msg.c_str());
+            return ret;
+        }
+    }
+    catch (const std::exception &e) {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_INTERNAL;
+        ret.msg        = e.what();
+        LOG_END_ABNORMAL(e);
+        return ret;
+    }
+}
+
+db_reply_t
+delete_asset_ext_attributes_with_ro (tntdb::Connection &conn,
+                                     uint32_t asset_element_id,
+                                     bool read_only)
+{
+    LOG_START;
+    log_debug ("read_only = %i, asset_element_id = %" PRIu32, read_only, asset_element_id);
+
+    db_reply_t ret = db_reply_new();
+
+    // input parameters control
+    if ( asset_element_id <= 0 )
+    {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_BADINPUT;
+        ret.msg        = "value <= 0 of asset_element_id is not allowed";
+        log_error ("end: %s, %s", "ignore delete", ret.msg.c_str());
+        return ret;
+    }
+    log_debug ("input parameters are correct");
+
+    try{
+        tntdb::Statement st = conn.prepareCached(
+            " DELETE FROM"
+            "   t_bios_asset_ext_attributes"
+            " WHERE"
+            "   id_asset_element = :element AND "
+            "   read_only = :ro "
+        );
+
+        ret.affected_rows = st.set("element", asset_element_id).
+                               set("ro", read_only).
+                               execute();
+        log_debug("[t_bios_asset_ext_attributes]: was deleted %"
+                                PRIu64 " rows", ret.affected_rows);
+        ret.status = 1;
+        LOG_END;
+        return ret;
+    }
+    catch (const std::exception &e) {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_INTERNAL;
+        ret.msg        = e.what();
+        LOG_END_ABNORMAL(e);
+        return ret;
+    }
+}
+
+db_reply_t
+delete_asset_element (tntdb::Connection &conn,
+                      uint32_t asset_element_id)
+{
+    LOG_START;
+    log_debug ("asset_element_id = %" PRIu32, asset_element_id);
+
+    db_reply_t ret = db_reply_new();
+
+    // input parameters control
+    if ( asset_element_id <= 0 )
+    {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_BADINPUT;
+        ret.msg        = "value <= 0 of asset_element_id is not allowed";
+        log_error ("end: %s, %s", "ignore delete", ret.msg.c_str());
+        return ret;
+    }
+    log_debug ("input parameters are correct");
+
+    try{
+        tntdb::Statement st = conn.prepareCached(
+            " DELETE FROM"
+            "   t_bios_asset_element"
+            " WHERE"
+            "   id_asset_element = :element"
+        );
+
+        ret.affected_rows  = st.set("element", asset_element_id).
+                                execute();
+        log_debug("[t_bios_asset_element]: was deleted %"
+                                PRIu64 " rows", ret.affected_rows);
+        if ( ( ret.affected_rows == 1 ) || ( ret.affected_rows == 0 ) )
+        {
+            ret.status = 1;
+            LOG_END;
+            return ret;
+        }
+        else
+        {
+            ret.status     = 0;
+            ret.errtype    = DB_ERR;
+            ret.errsubtype = DB_ERROR_DELETEFAIL;
+            ret.msg        = "unexpected number of rows was deleted";
+            log_error ("end: %s", ret.msg.c_str());
+            return ret;
+        }
+    }
+    catch (const std::exception &e) {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_INTERNAL;
+        ret.msg        = e.what();
+        LOG_END_ABNORMAL(e);
+        return ret;
+    }
+}
+
+db_reply_t
+delete_asset_element_from_asset_groups (tntdb::Connection &conn,
+                                        uint32_t asset_element_id)
+{
+    LOG_START;
+    log_debug ("asset_element_id = %" PRIu32, asset_element_id);
+
+    db_reply_t ret = db_reply_new();
+
+    // input parameters control
+    if ( asset_element_id <= 0 )
+    {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_BADINPUT;
+        ret.msg        = "value <= 0 of asset_element_id is not allowed";
+        log_error ("end: %s, %s", "ignore delete", ret.msg.c_str());
+        return ret;
+    }
+    log_debug ("input parameters are correct");
+
+    try{
+        tntdb::Statement st = conn.prepareCached(
+            " DELETE FROM"
+            "   t_bios_asset_group_relation"
+            " WHERE"
+            "   id_asset_element = :element"
+        );
+
+        ret.affected_rows = st.set("element", asset_element_id).
+                               execute();
+        log_debug("[t_bios_asset_group_relation]: was deleted %"
+                                PRIu64 " rows", ret.affected_rows);
+        ret.status = 1;
+        LOG_END;
+        return ret;
+    }
+    catch (const std::exception &e) {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_INTERNAL;
+        ret.msg        = e.what();
+        LOG_END_ABNORMAL(e);
+        return ret;
+    }
+}
+
+db_reply_t
+delete_asset_element_from_asset_group (tntdb::Connection &conn,
+                                       uint32_t asset_group_id,
+                                       uint32_t asset_element_id)
+{
+    LOG_START;
+    log_debug ("  asset_group_id = %" PRIu32, asset_group_id);
+    log_debug ("  asset_element_id = %" PRIu32, asset_element_id);
+
+    db_reply_t ret = db_reply_new();
+
+    // input parameters control
+    if ( asset_element_id <= 0 )
+    {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_BADINPUT;
+        ret.msg        = "value <= 0 of asset_element_id is not allowed";
+        log_error ("end: %s, %s", "ignore delete", ret.msg.c_str());
+        return ret;
+    }
+    if ( asset_group_id <= 0 )
+    {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_BADINPUT;
+        ret.msg        = "value <= 0 of asset_group_id is not allowed";
+        log_error ("end: %s, %s", "ignore delete", ret.msg.c_str());
+        return ret;
+    }
+    log_debug ("input parameters are correct");
+
+    try{
+        tntdb::Statement st = conn.prepareCached(
+            " DELETE FROM"
+            "   t_bios_asset_group_relation"
+            " WHERE"
+            "   id_asset_group = :grp AND"
+            "   id_asset_element = :element"
+        );
+
+        ret.affected_rows = st.set("grp", asset_group_id).
+                               set("element", asset_element_id).
+                               execute();
+        log_debug("[t_bios_asset_group_relation]: was deleted %"
+                                PRIu64 " rows", ret.affected_rows);
+        if ( ( ret.affected_rows == 1 ) || ( ret.affected_rows == 0 ) )
+        {
+            ret.status = 1;
+            LOG_END;
+            return ret;
+        }
+        else
+        {
+            ret.status     = 0;
+            ret.errtype    = DB_ERR;
+            ret.errsubtype = DB_ERROR_DELETEFAIL;
+            ret.msg        = "unexpected number of rows was deleted";
+            log_error ("end: %s", ret.msg.c_str());
+            return ret;
+        }
+    }
+    catch (const std::exception &e) {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_INTERNAL;
+        ret.msg        = e.what();
+        LOG_END_ABNORMAL(e);
+        return ret;
+    }
+}
+
+//TODO: inserted data are probably unused, check and remove
+db_reply_t
+delete_monitor_asset_relation_by_a (tntdb::Connection &conn,
+                                    uint32_t id)
+{
+    LOG_START;
+    log_debug ("  id = %" PRIu32, id);
+
+    db_reply_t ret = db_reply_new();
+
+    // input parameters control
+    if ( id <= 0 )
+    {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_BADINPUT;
+        ret.msg        = "value <= 0 of id is not allowed";
+        log_error ("end: %s, %s", "ignore delete", ret.msg.c_str());
+        return ret;
+    }
+    log_debug ("input parameters are correct");
+
+    try{
+        tntdb::Statement st = conn.prepareCached(
+            " DELETE FROM"
+            "   t_bios_monitor_asset_relation"
+            " WHERE"
+            "   id_asset_element = :id"
+        );
+
+        ret.affected_rows = st.set("id", id).
+                               execute();
+        log_debug("[t_bios_monitor_asset_relation]: was deleted %"
+                                PRIu64 " rows", ret.affected_rows);
+        ret.status = 1;
+        LOG_END;
+        return ret;
+    }
+    catch (const std::exception &e) {
+        ret.status     = 0;
+        ret.errtype    = DB_ERR;
+        ret.errsubtype = DB_ERROR_INTERNAL;
+        ret.msg        = e.what();
+        LOG_END_ABNORMAL(e);
+        return ret;
+    }
+}
+} // end namespace
